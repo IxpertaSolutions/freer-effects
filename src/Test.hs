c7bae4f7f40cd4655d8f5d2a620bf3dd49dfb53f
Sandy Maguire <sandy@sandymaguire.me>
2017-03-18 20:53:44 -0700

    clean up

--- src/Test.hs
+++ src/Test.hs
@@ -4,16 +4,14 @@
 
 module Test where
 
-
 import Control.Monad.Freer.TH
-import Control.Monad.Freer
 
 data MyEffect s a where
   WhatWhat :: s -> MyEffect s ()
   HowHow :: Int -> Bool -> s -> MyEffect s s
 
 
-liftInst ''MyEffect
+makeFreer ''MyEffect
 
 -- whatWhat :: (Member (MyEffect s) r) => s -> Eff r ()
 -- howHow :: (Member (MyEffect s) r) => Int -> Bool -> s -> Eff r s
