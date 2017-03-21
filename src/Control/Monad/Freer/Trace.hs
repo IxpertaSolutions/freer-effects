08a216df8c9986029b279eea674820e3a31a2836
Matej Kollar <208115@mail.muni.cz>
2017-03-21 22:27:08 +0100

    Generalize Trace effect
    Make Trace effect composable and make it possible to trace any value,
    not just String.
    
    Some convenient handlers provided.

--- src/Control/Monad/Freer/Trace.hs
+++ src/Control/Monad/Freer/Trace.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 -- |
 -- Module:       Control.Monad.Freer.Trace
@@ -20,27 +21,50 @@ module Control.Monad.Freer.Trace
     ( Trace(..)
     , trace
     , runTrace
+    , runTraceIO
+    , runTraceSilent
     )
   where
 
-import Control.Monad ((>>), return)
-import Data.Function ((.))
+import Control.Monad ((>>=), return)
+import Data.Either (Either(Left, Right))
+import Data.Function ((.), const)
 import Data.String (String)
 import System.IO (IO, putStrLn)
 
-import Control.Monad.Freer.Internal (Eff(E, Val), Member, extract, qApp, send)
+import Control.Monad.Freer.Internal
+    (Eff(E, Val), Member, decomp, qApp, send, tsingleton)
 
 
--- | A Trace effect; takes a 'String' and performs output.
-data Trace a where
-    Trace :: String -> Trace ()
+-- | A Trace effect.
+data Trace s a where
+    Trace :: s -> Trace s ()
 
 -- | Printing a string in a trace.
-trace :: Member Trace effs => String -> Eff effs ()
+trace :: Member (Trace s) effs => s -> Eff effs ()
 trace = send . Trace
 
--- | An 'IO' handler for 'Trace' effects.
-runTrace :: Eff '[Trace] a -> IO a
-runTrace (Val x) = return x
-runTrace (E u q) = case extract u of
-    Trace s -> putStrLn s >> runTrace (qApp q ())
+-- | Generic runner for 'Trace' effect.
+runTrace
+    :: (s -> Eff effs ())
+    -- ^ Function to trace 's' in terms of effect stack without 'Trace'.
+    -> Eff (Trace s ': effs) a -> Eff effs a
+runTrace _ (Val x) = return x
+runTrace f (E u q) = case decomp u of
+    Right (Trace s) -> f s >>= runTrace f . qApp q
+    Left u' -> E u' (tsingleton (runTrace f . qApp q))
+
+-- | Simple runner for 'Trace String'.
+runTraceIO :: Member IO effs => Eff (Trace String ': effs) a -> Eff effs a
+runTraceIO = runTrace (send . putStrLn)
+
+-- | Ignore traces.
+runTraceSilent
+    :: forall proxy effs s a
+    . proxy s
+    -- ^ Proxy for what traces to throw away.
+    -> Eff (Trace s ': effs) a -> Eff effs a
+runTraceSilent p = runTrace (f p)
+  where
+    f :: proxy s -> s -> Eff effs ()
+    f _ = const (return ())
