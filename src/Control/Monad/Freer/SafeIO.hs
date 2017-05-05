17f7a06bad6227e7959f96651c1209cc1b168dad
Sandy Maguire <sandy@sandymaguire.me>
2017-05-05 12:11:08 -0600

    rename doIO to safely

--- src/Control/Monad/Freer/SafeIO.hs
+++ src/Control/Monad/Freer/SafeIO.hs
@@ -18,7 +18,7 @@ import Control.Exception
 ------------------------------------------------------------------------------
 -- | Safe IO effect.
 data SIO a where
-  DoIO :: IO a -> SIO (Either SomeException a)
+  Safely :: IO a -> SIO (Either SomeException a)
 
 
 ------------------------------------------------------------------------------
@@ -27,13 +27,13 @@ data SIO a where
 -- exceptions left unhandled in this effect will be rethrown in IO.
 runSafeIO :: Eff '[SIO, Exc SomeException] w -> IO w
 runSafeIO (Val x) = return x
-runSafeIO (E u q) | Just (DoIO m) <- prj u = try m >>= runSafeIO . qApp q
-runSafeIO (E u _) | Just (Exc e)  <- prj u = throw (e :: SomeException)
+runSafeIO (E u q) | Just (Safely m) <- prj u = try m >>= runSafeIO . qApp q
+runSafeIO (E u _) | Just (Exc e)    <- prj u = throw (e :: SomeException)
 runSafeIO (E _ _) = error "can't happen"
 
 
 ------------------------------------------------------------------------------
 -- | Lift an 'IO' action to the 'SIO' effect.
 safeIO :: (Member SIO r, Member (Exc SomeException) r) => IO a -> Eff r a
-safeIO io = either throwError return =<< send (DoIO io)
+safeIO io = either throwError return =<< send (Safely io)
 
