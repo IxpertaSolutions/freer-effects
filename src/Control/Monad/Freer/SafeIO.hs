0dae1af5ebe851e47d8767934779ec5ef3f6501a
Sandy Maguire <sandy@sandymaguire.me>
2017-05-05 12:09:21 -0600

    comment safe io

--- src/Control/Monad/Freer/SafeIO.hs
+++ src/Control/Monad/Freer/SafeIO.hs
@@ -3,7 +3,11 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Control.Monad.Freer.SafeIO where
+module Control.Monad.Freer.SafeIO
+  ( SIO ()
+  , runSafeIO
+  , safeIO
+  ) where
 
 import Control.Monad.Freer
 import Control.Monad.Freer.Internal
@@ -11,15 +15,25 @@ import Control.Monad.Freer.Exception
 import Control.Exception
 
 
+------------------------------------------------------------------------------
+-- | Safe IO effect.
 data SIO a where
   DoIO :: IO a -> SIO (Either SomeException a)
 
+
+------------------------------------------------------------------------------
+-- | Interprets 'SIO' into 'IO'. There is an @Exc SomeException@ *underneath*
+-- the safe IO effect here to ensure we always have it in our effect stack. Any
+-- exceptions left unhandled in this effect will be rethrown in IO.
 runSafeIO :: Eff '[SIO, Exc SomeException] w -> IO w
 runSafeIO (Val x) = return x
 runSafeIO (E u q) | Just (DoIO m) <- prj u = try m >>= runSafeIO . qApp q
 runSafeIO (E u _) | Just (Exc e)  <- prj u = throw (e :: SomeException)
 runSafeIO (E _ _) = error "can't happen"
 
+
+------------------------------------------------------------------------------
+-- | Lift an 'IO' action to the 'SIO' effect.
 safeIO :: (Member SIO r, Member (Exc SomeException) r) => IO a -> Eff r a
 safeIO io = either throwError return =<< send (DoIO io)
 
